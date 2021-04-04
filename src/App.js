c200a99547fe1812c1d0d0ce57c37fb4890a08e1
johnniesimpson <johnniesimpson@outlook.com>
2021-04-04 11:48:21 -0400

    Install dependencies. Update App and source index to remove redundancies.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+import React from "react";
+import {BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import Header from "./components/Header/index";
+import Footer from "./components/Footer/index";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>

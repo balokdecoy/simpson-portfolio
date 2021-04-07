815b86a22cdac8e5af5337a45eeaa39a440b7eb8
johnniesimpson <johnniesimpson@outlook.com>
2021-04-06 23:20:11 -0400

    Update public index file with CDNs. Continuing to add content to home page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,11 @@ function Home() {
     return (
     <Container>
         <Row>
-          <Col size="md-6">
+          <Col size="md-8">
           <h1 style={{paddingTop: "30px", color: "white", fontFamily: "Inconsolata, monospace"}}> {">"} hello, world...|</h1>
                     <div className="row">
                         <div className="col-md-10">
-                            <div id="myBio" style={{color: "lime"}}>
+                            <div id="myBio" style={{color: "#a3cc00", fontFamily: "Inconsolata, monospace"}}>
                                 Welcome. I'm Johnnie Simpson, I am a full-stack web developer from 
                                 Baltimore, Maryland. I am currently enrolled in the George Washington University 
                                 Full Stack Coding Bootcamp. I have a B.A. in English Language and Literature from
@@ -24,6 +24,34 @@ function Home() {
                                 on the Portfolio tab. You can reach me directly at the Contact Me tab, or on
                                 social media. 
                             </div>
+                        </div>
+                    </div>
+          </Col>
+          <Col size="md-4" style={{paddingTop:"30px"}}>
+          <div class="card text-white bg-dark mb-3" style={{maxWidth: "18rem"}}>
+                        <img class="card-img-top" src="./images/images/me.jpg" alt="my picture" />
+                        <div class="card-body">
+                            <h5 class="card-title">let's connect</h5>
+                            <p class="card-text">
+                                <a class="button" href="https://github.com/balokdecoy" target="_blank" 
+                                    style={{color:"white", padding: "5px"}}>    
+                                    <i class="fab fa-github fa-3x"></i>
+                                </a>
+                                <a class="button" href="./assets/resume/johnnie-simpson-resume-2021.pdf" target="_blank" 
+                                style={{color:"red", padding: "5px"}}>
+                                <i class="far fa-file-pdf fa-3x"></i>
+                                </a>
+                                <a class="button" href="https://www.linkedin.com/in/johnnie-simpson-jr-46629586/"
+                                    target="_blank" style={{color: "blue", padding: "5px"}}>
+                                    <i class="fab fa-linkedin fa-3x"></i>
+                                </a>
+                                <a class="button" href="https://www.youtube.com/channel/UCH4BSKNihbY1rpfJSUrR7Pw/featured" target="_blank" 
+                                    style={{color:"red", padding: "5px"}}>
+                                    <i class="fab fa-youtube fa-3x"></i>
+                                </a>
+                                <p>Phone: <br /> 443-974-3428</p>
+                                <p>Email: johnniesimpson88@gmail.com</p>
+                            </p>
                         </div>
                     </div>
           </Col>

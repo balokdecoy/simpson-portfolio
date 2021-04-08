7ea6c1a440697fe711786ace237a1260754253b0
johnniesimpson <johnniesimpson@outlook.com>
2021-04-08 18:21:36 -0400

    Created Contact page and linked it to the App file.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
-import Portfolio from "./pages/Portfolio"
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
 import Footer from "./components/Footer/index";
 import "./App.css";
 
@@ -14,6 +15,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/Portfolio" component={Portfolio} />
+          <Route exact path="/Contact" component={Contact} />
         </Switch>
         <Footer />
       </div>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,3 +1,40 @@
 import React from "react";
 import { Col, Row, Container } from "../components/Grid/index";
+import Form from 'react-bootstrap/Form';
+import FormFile from 'react-bootstrap/FormFile'
 
+
+function Contact() {
+    return (
+        <Container>
+            <Row>
+                <Col size="md-6">
+                <Form>
+                        <div className="form-group">
+                          <h1 style={{paddingTop: "30px", color: "white"}}>contact me</h1>  
+                          <br />
+                          <label htmlFor="exampleFormControlInput1">first name</label>
+                          <input type="text" className="form-control" placeholder="first name" />
+                        </div>
+                        <div className="form-group"> 
+                            <label>last name</label>
+                            <input type="text" className="form-control" placeholder="last name" />
+                        </div>
+                        <div className="form-group"> 
+                            <label>email address</label>
+                            <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+                        </div>
+                        <div className="form-group">
+                          <label>message</label>
+                          <textarea className="form-control" rows="3" 
+                          placeholder="what's up?"></textarea>
+                        </div>
+                        <button type="submit" className="btn btn-primary">submit</button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default Contact;

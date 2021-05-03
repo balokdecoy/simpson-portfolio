c0ab8136b86821c1a9ddc63917621c14a8f14a7c
johnniesimpson <johnniesimpson@outlook.com>
2021-05-03 19:17:50 -0400

    Correct color on contact page.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -13,19 +13,19 @@ function Contact() {
                         <div className="form-group">
                           <h1 style={{paddingTop: "30px", color: "white"}}>contact me</h1>  
                           <br />
-                          <label htmlFor="exampleFormControlInput1">first name</label>
+                          <label htmlFor="exampleFormControlInput1" style={{color:"white"}}>first name</label>
                           <input type="text" className="form-control" placeholder="first name" />
                         </div>
                         <div className="form-group"> 
-                            <label>last name</label>
+                            <label style={{color:"white"}}>last name</label>
                             <input type="text" className="form-control" placeholder="last name" />
                         </div>
                         <div className="form-group"> 
-                            <label>email address</label>
+                            <label style={{color:"white"}}>email address</label>
                             <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
                         </div>
                         <div className="form-group">
-                          <label>message</label>
+                          <label style={{color:"white"}}>message</label>
                           <textarea className="form-control" rows="3" 
                           placeholder="what's up?"></textarea>
                         </div>

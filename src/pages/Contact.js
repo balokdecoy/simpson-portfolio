4939bb0b0e324fde3b144f9d1a5020514854bcd6
johnniesimpson <johnniesimpson@outlook.com>
2021-05-06 17:57:04 -0400

    Working on Contact Form features. Added a React alert letting users know the contact page is currently in development.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Col, Row, Container } from "../components/Grid/index";
 import Form from 'react-bootstrap/Form';
-import FormFile from 'react-bootstrap/FormFile'
-
+import FormFile from 'react-bootstrap/FormFile';
+import{ init } from 'emailjs-com';
+init("user_mOQiDnOMdIxqFfHkwIVUx");
 
 function Contact() {
+
+    const [name, setName] = useState([]);
+
+    const [email, setEmail] = useState([]);
+
+    const [message, setMessage] = useState([]);
+
+    const handleChange = (e) => {
+        setName({value: e.target.value})
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(name)
+        alert("Contact Form is currently under development. Check back later!")
+    }
+
     return (
         <Container>
             <Row>
                 <Col size="md-6">
-                <Form>
+                <Form onSubmit={handleSubmit}>
                         <div className="form-group">
                           <h1 style={{paddingTop: "30px", color: "white"}}>contact me</h1>  
                           <br />
-                          <label htmlFor="exampleFormControlInput1" style={{color:"white"}}>first name</label>
-                          <input type="text" className="form-control" placeholder="first name" />
-                        </div>
-                        <div className="form-group"> 
-                            <label style={{color:"white"}}>last name</label>
-                            <input type="text" className="form-control" placeholder="last name" />
+                          <label htmlFor="exampleFormControlInput1" style={{color:"white"}}>your name</label>
+                          <input type="text" className="form-control" placeholder="your name here" 
+                          onChange={handleChange}/>
                         </div>
                         <div className="form-group"> 
                             <label style={{color:"white"}}>email address</label>
@@ -29,7 +44,7 @@ function Contact() {
                           <textarea className="form-control" rows="3" 
                           placeholder="what's up?"></textarea>
                         </div>
-                        <button type="submit" className="btn btn-primary">submit</button>
+                        <button type="submit" value="Submit" className="btn btn-primary">submit</button>
                     </Form>
                 </Col>
             </Row>
